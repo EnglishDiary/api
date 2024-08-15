02776baf39090d46614fefc856fc30f8d053bacc
94peterchoi <94peterchoi@gmail.com>
2024-08-15 21:51:21 +0900

    Feat: Implement global error handler for exceptions

---
+++ src/main/java/org/eng_diary/api/exception/GlobalErrorHandler.java
@@ -0,0 +1,24 @@
+package org.eng_diary.api.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eng_diary.api.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error("unknown error", e);
+
+        ApiResponse<?> error = ApiResponse.error("알 수 없는 오류가 발생하였습니다.");
+        return new ResponseEntity<>(error, INTERNAL_SERVER_ERROR);
+    }
+
+}
