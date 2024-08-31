1818e2b1aecb664aee24a96dd1ad5b4c78ff16ab
94peterchoi <94peterchoi@gmail.com>
2024-08-31 15:06:33 +0900

    Feat: Create base entity for handling register and update time

--- src/main/java/org/eng_diary/api/business/word/dto/WordSaveRequest.java
+++ src/main/java/org/eng_diary/api/business/word/dto/WordSaveRequest.java
@@ -3,11 +3,14 @@ package org.eng_diary.api.business.word.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class WordSaveRequest {
 
     private String jsonData;
     private Long categoryId;
+    private LocalDateTime registerTime;
 
 }
--- src/main/java/org/eng_diary/api/domain/MemberWord.java
+++ src/main/java/org/eng_diary/api/domain/MemberWord.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-public class MemberWord {
+public class MemberWord extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
