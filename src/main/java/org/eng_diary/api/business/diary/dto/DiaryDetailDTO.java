caa413d530da57256e433b912f2f370f9d4c45d9
94peterchoi <94peterchoi@gmail.com>
2024-09-08 18:36:33 +0900

    feat: create DTO class for diary details

---
+++ src/main/java/org/eng_diary/api/business/diary/dto/DiaryDetailDTO.java
@@ -0,0 +1,36 @@
+package org.eng_diary.api.business.diary.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DiaryDetailDTO {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String aiRevisedDiary;
+
+    private String aiFeedback;
+
+    private LocalDateTime registerTime;
+
+    private String memberName;
+
+    private String memberProfileUrl;
+
+    private String thumbnailUrl;
+
+    private Integer likes;
+
+    private Integer comments;
+
+
+
+}
