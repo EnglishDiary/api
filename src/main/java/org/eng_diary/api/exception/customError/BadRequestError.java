2ef81416efc0831702b15ed325419949f9fc0faf
94peterchoi <94peterchoi@gmail.com>
2024-09-15 21:42:30 +0900

    add: Add response status to the error class

--- src/main/java/org/eng_diary/api/exception/customError/BadRequestError.java
+++ src/main/java/org/eng_diary/api/exception/customError/BadRequestError.java
@@ -1,5 +1,9 @@
 package org.eng_diary.api.exception.customError;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestError extends RuntimeException {
 
     public BadRequestError(String message) {
