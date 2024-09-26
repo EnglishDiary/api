ef793d22968690e9b31250bb671ddf2136dc3e58
94peterchoi <94peterchoi@gmail.com>
2024-09-26 15:55:03 +0900

    feat: implement to retrive file meta data

---
+++ src/main/java/org/eng_diary/api/business/filemng/repository/FileManagerRepository.java
@@ -0,0 +1,30 @@
+package org.eng_diary.api.business.filemng.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.eng_diary.api.domain.FileMeta;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static org.eng_diary.api.domain.QFileMeta.fileMeta;
+
+@Repository
+public class FileManagerRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public FileManagerRepository(EntityManager em) {
+        this.em = em;
+        queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<FileMeta> findFileMetaList(String tableName, List<Long> ids) {
+        return queryFactory.selectFrom(fileMeta)
+                .where(fileMeta.referencedTable.eq(tableName))
+                .where(fileMeta.id.in(ids))
+                .fetch();
+    }
+
+}
