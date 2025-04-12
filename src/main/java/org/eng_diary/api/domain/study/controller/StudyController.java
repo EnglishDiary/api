1ec4bd38c1e3f1c469008232fe9557a610037aa6
94peterchoi <94peterchoi@gmail.com>
2025-04-12 19:28:47 +0900

    feat: add new api

--- src/main/java/org/eng_diary/api/domain/study/controller/StudyController.java
+++ src/main/java/org/eng_diary/api/domain/study/controller/StudyController.java
@@ -1,7 +1,22 @@
 package org.eng_diary.api.domain.study.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.eng_diary.api.domain.study.service.AiService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/study")
 public class StudyController {
+
+    private final AiService aiService;
+
+    @PostMapping("/expression")
+    public void analyzeExpression() {
+        aiService.studyExpression(1L);
+    }
+
+
 }
