92336cb7e2f354a82af8ec621c3190e730ef1924
94peterchoi <94peterchoi@gmail.com>
2025-04-20 06:17:51 +0900

    refactor: remove configuration file that registers interceptor
    - reuse exited configuration file that implements the same configurer

--- src/main/java/org/eng_diary/api/config/WebMvcConfig.java
+++ src/main/java/org/eng_diary/api/config/WebMvcConfig.java
@@ -1,14 +1,19 @@
 package org.eng_diary.api.config;
 
+import lombok.RequiredArgsConstructor;
+import org.eng_diary.api.common.interceptor.JwtAuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
+    private final JwtAuthenticationInterceptor jwtAuthenticationInterceptor;
 
     @Value("${app.cors.allowedOrigins}")
     private String[] allowedOrigins;
@@ -22,4 +27,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtAuthenticationInterceptor)
+                .addPathPatterns("/**") // API 경로에 인터셉터 적용
+                // 공개 API 제외
+                .excludePathPatterns("/user/login")
+        ;
+//                .excludePathPatterns("/user/me");
+    }
+
 }
