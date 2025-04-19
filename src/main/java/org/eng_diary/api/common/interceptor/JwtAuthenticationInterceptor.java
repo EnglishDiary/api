2ff9746d436821edb49a28bbb07c62e215bbee42
94peterchoi <94peterchoi@gmail.com>
2025-04-20 06:10:07 +0900

    feat: always allow preflight request

--- src/main/java/org/eng_diary/api/common/interceptor/JwtAuthenticationInterceptor.java
+++ src/main/java/org/eng_diary/api/common/interceptor/JwtAuthenticationInterceptor.java
@@ -16,6 +16,9 @@ public class JwtAuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true; // OPTIONS 요청은 항상 허용
+        }
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
