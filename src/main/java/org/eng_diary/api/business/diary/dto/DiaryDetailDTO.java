1cb755174855e7a715149a51d841b4d4488a1445
SamChoi <94peterchoi@gmail.com>
2025-08-05 22:08:54 +0900

    update: add files field

--- src/main/java/org/eng_diary/api/business/diary/dto/DiaryDetailDTO.java
+++ src/main/java/org/eng_diary/api/business/diary/dto/DiaryDetailDTO.java
@@ -2,8 +2,11 @@ package org.eng_diary.api.business.diary.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.eng_diary.api.business.filemanage.dto.response.FileMetaResponse;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -31,6 +34,6 @@ public class DiaryDetailDTO {
 
     private Integer comments;
 
-
+    private List<FileMetaResponse> files = new ArrayList<>();
 
 }
