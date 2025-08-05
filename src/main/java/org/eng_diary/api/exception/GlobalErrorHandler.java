f5b977b8c26b54ea98bc7bcb7e74c3e2888c352d
SamChoi <94peterchoi@gmail.com>
2025-08-05 22:04:30 +0900

    refactor: add handler about jwt-expiration

--- src/main/java/org/eng_diary/api/exception/GlobalErrorHandler.java
+++ src/main/java/org/eng_diary/api/exception/GlobalErrorHandler.java
@@ -1,5 +1,6 @@
 package org.eng_diary.api.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.eng_diary.api.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,14 @@ import static org.springframework.http.HttpStatus.*;
 @RestControllerAdvice
 public class GlobalErrorHandler {
 
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<?> handleExpiredJwtException(ExpiredJwtException e) {
+        log.error("User Token is Expired", e);
+
+        ApiResponse<?> response = ApiResponse.error("토큰 유효시간이 만료되었습니다.");
+        return new ResponseEntity<>(response, UNAUTHORIZED);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e) {
         log.error("unknown error", e);
@@ -19,7 +28,5 @@ public class GlobalErrorHandler {
         ApiResponse<?> error = ApiResponse.error("알 수 없는 오류가 발생하였습니다.");
         return new ResponseEntity<>(error, INTERNAL_SERVER_ERROR);
     }
-
-    // TODO 250420 JWT 토큰 예외처리 (만료, 유효하지 않은 토큰값 등)
 
 }
